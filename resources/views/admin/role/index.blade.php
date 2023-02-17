0fa4cd74effd4c88ec89427c2bcef8d69881a8bb
saliou33 <losaliou.sl33@gmail.com>
2023-02-17 22:18:50 +0000

    Fix Typos

--- resources/views/admin/role/index.blade.php
+++ resources/views/admin/role/index.blade.php
@@ -54,7 +54,7 @@
             <div class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
             <div class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
                 <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalCenteredScrollableLabel">
-                    Creer Une Categorie
+                    Creer Un Role
                 </h5>
                 <button type="button"
                 class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
